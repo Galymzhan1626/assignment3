21e1828be881344ef3a9d77a313a5d00af40d933
Galymzhan1626 <231169@astanait.edu.kz>
2024-05-17 13:22:46 +0500

    assignment3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
+import com.sun.source.tree.BinaryTree;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+
+        binaryTree.insert(5);
+        binaryTree.insert(3);
+        binaryTree.insert(4);
+        binaryTree.insert(2);
+        binaryTree.insert(6);
+        binaryTree.insert(7);
+
+        binaryTree.inOrder();
+        System.out.println();
+        System.out.println("Root data before remove: " +binaryTree.root.data);
+        binaryTree.remove(5);
+        System.out.println();
+        binaryTree.inOrder();
+        System.out.println();
+        System.out.println("Root data after remove: " +binaryTree.root.data);
     }
 }
---
+++ src/MyHashTable.java
@@ -0,0 +1,39 @@
+public class MyHashTable<K, V> {
+
+    private static final int initialSize=16;
+    private MyHashNode<K,V>[] chainArr;
+    private int size;
+
+    public class MyHashNode<K, V> {
+        private K key;
+        private V value;
+        private MyHashNode<K,V> next;
+
+        public MyHashNode(K key, V value){
+            this.key=key;
+            this.value=value;
+        }
+
+        @Override
+        public String toString() {
+            return "MyHashNode{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    };
+    public MyHashTable(){
+        this(initialSize);
+    }
+    public MyHashTable(int givenSize){
+        chainArr=new MyHashNode[givenSize];
+        size=0;
+    }
+    private int hash(K key) {};
+    public void put(K key, V value) {};
+    public V get(K key) {};
+    public V remove(K key) {};
+    public boolean contains(V value) {};
+    public K getKey(V value){};
+}
